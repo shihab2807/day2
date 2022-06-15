c268ce7b7b55b820dba9b93570057316c3c0269c
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:29:07 +0100

    day2 done

---
+++ day2/src/day2/demo7.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class demo7 {
+
+	public static void main(String[] args) {
+		int total = 90;
+		float marks = 72.3f ;
+		float percentage;
+		percentage = (marks/total)*100;
+		System.out.println("percentage= " +percentage );
+		// TODO Auto-generated method stub
+
+	}
+
+}
