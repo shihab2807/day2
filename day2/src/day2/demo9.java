bcc205ae31d14f30c32fa6d3fda6098da3884f5f
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:33:04 +0100

    day2 done

---
+++ day2/src/day2/demo9.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class demo9 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		if (a !=10)
+		{
+			System.out.println("wrong value");
+			
+		}
+		else
+		{
+			System.out.println("true value");
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
