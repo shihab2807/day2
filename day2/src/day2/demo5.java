9aee535f2aabdb6a63307ff9ace27b329d6c7a70
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:24:51 +0100

    day2 done

---
+++ day2/src/day2/demo5.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class demo5 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		a+= 20;
+		System.out.println(a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
