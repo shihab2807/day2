41c228845a24c9223be9f70bc62a387fb5c8a148
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:20:41 +0100

    day2 done

---
+++ day2/src/day2/demo4.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		float f = 10.2525f;
+		int l = (int)f;
+		System.out.println(l);
+		// TODO Auto-generated method stub
+
+	}
+
+}
