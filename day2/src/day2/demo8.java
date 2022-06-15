9bdec393713530a36953f27430340da7cab013d3
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:31:24 +0100

    day2 done

---
+++ day2/src/day2/demo8.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class demo8 {
+
+	public static void main(String[] args) {
+		String name = "Ankesh";
+		String surname ="Gupta";
+		String full_name = name+" "+surname;
+		System.out.println(full_name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
