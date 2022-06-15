4cc816141e69d582b8750a1a8b1d56e84d7234ea
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:37:40 +0100

    day2 done

---
+++ day2/src/day2/day11.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class day11 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		if((a<b)&&(a==b))
+		{
+			System.out.println("TRUE");
+		}
+		else
+		{
+			System.out.println("FALSE");
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
