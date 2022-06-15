4640d5c3c559e093cd05928069f6f7c4e8eec45b
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-15 11:11:16 +0100

    day2 done

---
+++ day2/src/day2/Demo2.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(++b);
+		System.out.println(--a);
+
+
+	}
+
+}
